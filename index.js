4e123ae574b6a7c1a0cc7d52743ad11d1940217f
Kator Agashua <katoragashua@outlook.com>
2022-06-19 10:23:17 +0100

    check

--- index.js
+++ index.js
@@ -4,19 +4,37 @@ const svgLeftMobile = document.getElementById("svg-left-mobile");
 const svgRightMobile = document.getElementById("svg-right-mobile");
 const workAboutSvg = document.getElementById("svg-work-about");
 const workAboutSvgMobile = document.getElementById("svg-work-about-mobile");
+const svgFooter = document.getElementById("svg-footer");
+const svgFooterMobile = document.getElementById("svg-footer-mobile");
 workAboutSvg.classList.add("svg-work-about");
+const hamburger = document.getElementById("hamburger");
 
+const menu = document.getElementById("menu");
 console.log(svgLeft, svgRight);
-
-const viewPort = window.innerWidth;
+const mobile = 450;
+const desktop = 451;
+let viewPort = document.body.clientWidth;
 console.log(viewPort);
 
-if(viewPort <= 450) {
-    svgRight.style.display = "none";
-    svgLeft.style.display = "none";
-    workAboutSvg.style.display = 'none';
-} else if(viewPort > 450) {
-    svgRightMobile.style.display = "none";
-    svgLeftMobile.style.display = "none";
-    workAboutSvgMobile.style.display = 'none'
+window.addEventListener("resize", resize)
+
+function resize() {
+    if (viewPort <= mobile) {
+      svgRight.style.display = "none";
+      svgLeft.style.display = "none";
+      workAboutSvg.style.display = "none";
+      svgFooter.style.display = "none";
+    } else if (viewPort >= desktop) {
+      svgRightMobile.style.display = "none";
+      svgLeftMobile.style.display = "none";
+      workAboutSvgMobile.style.display = "none";
+      svgFooterMobile.style.display = "none";
+    }
 }
+
+resize()
+
+hamburger.addEventListener("click", (e) => {
+    menu.classList.toggle("slide-in");
+    console.log(menu)
+});
